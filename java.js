f2844920c09a328c43b4bf6ad11e45be08474987
Nishant Singh <755083@student.tor.loc>
2019-06-19 16:36:29 -0400

    3 commit

--- java.js
+++ java.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const circle = {
+    radius: 1,
+    location: {
+
+        x: 1,
+        y: 1
+
+    },
+    draw: function() {
+        console.log('draw');
+
+    }
+
+};
---
+++ java1.js
@@ -0,0 +1,22 @@
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+const circle = createCircle(1);
+circle.draw();
+
+// Constructor Function
+
+function Circle(radius) {
+    console.log('this', this);
+    this.radius = radius;
+    this.draw = function() {
+        console.log("draw");
+    }
+}
+
+const another = new Circle(1);
